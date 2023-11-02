889d4129795bc8321a2b13c3236e1ad79076ed00
DispicableLee <lee.robert053@gmail.com>
2023-11-02 16:55:28 -0400

    trying to create

--- pokedex-rails-backend/app/controllers/api/pokemon_controller.rb
+++ pokedex-rails-backend/app/controllers/api/pokemon_controller.rb
@@ -14,4 +14,23 @@ class Api::PokemonController < ApplicationController
         # render json: @pokemon
     end
 
+    def create
+        @new_pokemon = Pokemon.new(pokemon_params)
+        @new_pokemon.moves = params[:moves]
+        if @new_pokemon.save
+            render json: @new_pokemon
+        else
+            render json: @new_pokemon.errors.full_messages
+        end
+    end
+
+
+
+
+    private
+    def pokemon_params
+        params.require(:pokemon).permit(:number, :attack, :defense, :image_url, :name, :poke_type)
+    end
+
+
 end
